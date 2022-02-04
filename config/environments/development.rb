a01aa1dab058bb35410a8d8434f9e8f9be3e8d62
Alex McConnell <alexmmcconnell@gmail.com>
2022-02-04 15:20:20 -0700

    Feat - Add ActionMailer configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,16 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['user_email'],
+    password: ENV['user_code'],
+    enable_startls_auto: true
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
