911a8f364122d43cb9d39e81547c3de470778a78
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-02-04 17:30:36 -0700

    Add email verification and extra conditionals

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+  
   def create
-    user_info = request.POST
     user = User.new(user_params)
-    if user.save
+    if !user.email.include?('@')
+      render json: {error: "Error creating user. A valid email must be provided."}, status: 400
+    elsif user.save
       UserMailer.registration(user).deliver_now
-      render json: "Success", status: 200
+      render json: {success: "User created. A confirmation email has been sent to the user."}, status: 200
     else
-      render json: "Fail", status: 400
+      render json: {error: "Error creating user."}, status: 400
     end
   end
 
