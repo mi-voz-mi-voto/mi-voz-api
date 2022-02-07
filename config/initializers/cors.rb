86494098e71b5ef25cc7cd14755c243bfb8e3341
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-02-07 09:31:19 -0700

    comment out cors for setup later

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,15 +5,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:3001'
-
-    resource '/users',
-      headers: :any,
-      methods: [:post, :delete]
-      resource '/elections',
-        headers: :any,
-        methods: [:get]
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'http://localhost:3001'
+#
+#     resource '/users',
+#       headers: :any,
+#       methods: [:post, :delete]
+#       resource '/elections',
+#         headers: :any,
+#         methods: [:get]
+#   end
+# end
