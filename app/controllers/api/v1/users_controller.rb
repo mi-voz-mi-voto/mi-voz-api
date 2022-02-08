47b6a72a82ef0dede12ced21774c362ace462dd5
Alex McConnell <alexmmcconnell@gmail.com>
2022-02-08 14:43:57 -0700

    Refactor - Adjust error messages and user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,13 +3,16 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    
     if !user.email.include?('@')
       render json: {error: "Error creating subscriber. A valid email must be provided."}, status: 400
     elsif user.save
       UserMailer.registration(user).deliver_now
       render json: {success: "You are now registered to receive notifications about upcoming elections in your state. A confirmation email has been sent to #{user.email}."}, status: 200
+    elsif User.find_by(email: user.email).present?
+      render json: {error: "This email is already susbscribed to receive election notifications."}, status: 400
     else
-      render json: {error: "Error creating subscriber."}, status: 400
+      render json: {error: "Error creating subscriber. Please ensure all elements in the form are filled out correctly."}, status: 400
     end
   end
 
@@ -32,6 +35,6 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :language, :state)
+    params.require(:user).permit(:first_name, :last_name, :email, :language, :state_name)
   end
 end
