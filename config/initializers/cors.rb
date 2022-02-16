38d95f5ecf50274fc9b4ed3f0297a31315299101
Alex McConnell <alexmmcconnell@gmail.com>
2022-02-16 15:05:21 -0700

    Feat - Add FE Heroku app to allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000',
+            'https://mi-voz-mi-voto.herokuapp.com/'
 
     resource '/users',
       headers: :any,
