73068d32dfbd1c57611a4493747762ccfddc3fb3
Alex McConnell <alexmmcconnell@gmail.com>
2022-02-17 09:13:17 -0700

    Refactor - Allow all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000',
-            'https://mi-voz-mi-voto.herokuapp.com/'
+    origins '*'
 
     resource '/users',
       headers: :any,
