45ec0f12091f7111d8bd8569a04745c602946bbd
Luis Arroyo <arroyoluis1516@yahoo.com>
2022-02-17 11:34:18 -0700

    allow any and all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,16 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
-    resource '/users',
-      headers: :any,
-      resource: '*',
-      expose: ['Access-Control-Allow-Origin'],
-      methods: [:get, :post, :delete, :patch]
-    resource '/elections',
-      headers: :any,
-      resource: '*',
-      expose: ['Access-Control-Allow-Origin'],
-      methods: [:get]
+       resource(
+         '*',
+         headers: :any,
+         methods: [:get, :patch, :put, :delete, :post, :options]
+         )
   end
 end
